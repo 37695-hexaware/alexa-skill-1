91169731de4e09dbec1d12732c307bf2a2d16f51
Vinodkumar Puliyadi <34186@Hexaware.com>
2017-07-06 14:56:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const restService = express();
+restService.use(bodyParser.json());
+
+restService.post('/hook', function (req, res) {
+
+    console.log('hook request');
+
+});
+
+restService.listen((process.env.PORT || 5000), function () {
+    console.log("Server listening");
+});
